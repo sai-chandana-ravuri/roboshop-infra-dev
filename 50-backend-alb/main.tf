b5cadaed9eb4decbffcae336f0816f06d7e05a3e
sai-chandana-ravuri <saichandanaravuri@gmail.com>
2026-06-05 00:29:44 +0530

    Created backend-alb

---
+++ 50-backend-alb/main.tf
@@ -0,0 +1,48 @@
+resource "aws_lb" "backend_alb" {
+  name               = "${var.project_name}-${var.environment}"
+  internal           = true
+  load_balancer_type = "application"
+  security_groups    = [local.backend_alb_sg_id]
+  subnets            = local.private_subnet_ids
+
+# Just keeping it as false, just to delete using terraform while practice
+  enable_deletion_protection = false
+
+  tags =  merge(
+        {
+            Name = "${var.project_name}-${var.environment}"
+        },
+        local.common_tags
+    )
+}
+
+#listener
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.front_end.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "<h1>Hi, I am from Backend-ALB.</h1>"
+      status_code  = "200"
+    }
+  }
+}
+
+#record for backend-alb
+resource "aws_route53_record" "www" {
+  zone_id = var.zone_id
+  name    = "*.backend-alb-${var.environment}.${var.domain_name}"
+  type    = "A"
+
+  # load balancer details for route 53
+  alias {
+    name                   = aws_lb.backend_alb.dns_name
+    zone_id                = aws_lb.backend_alb.zone_id
+    evaluate_target_health = true
+  }
+}
---
+++ 50-backend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z08145131ZQYG4A7JX2E8"
+}
+
+variable "domain_name" {
+    default = "daws88c.online"
+}
