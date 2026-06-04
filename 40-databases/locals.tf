4358f564d7dd10196cdf1acaa1ce7cb72349105c
sai-chandana-ravuri <saichandanaravuri@gmail.com>
2026-06-04 16:50:49 +0530

    roboshop-infra-dev

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -3,6 +3,8 @@ locals {
     database_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
     redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
+    mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
+    rabbitmq_sg_id = data.aws_ssm_parameter.rabbitmq_sg_id.value
     common_tags = {
         Project = var.project_name
         Environment = var.environment
--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -79,3 +79,83 @@ resource "terraform_data" "bootstrap_redis" {
   }
 }
 
+#mysql
+resource "aws_instance" "mysql" {
+    ami           = local.ami_id
+    instance_type = "t3.micro"
+    subnet_id = local.database_subnet_id
+    vpc_security_group_ids = [local.mysql_sg_id]
+    tags = merge(
+        {
+            Name = "${var.project_name}-${var.environment}-mysql"
+        },
+        local.common_tags,
+    )
+}
+
+
+resource "terraform_data" "bootstrap_mysql" {
+  triggers_replace = [
+    aws_instance.mysql.id
+  ]
+
+ connection {
+    type = "ssh"
+    user = "ec2-user"
+    password = "DevOps321"
+    host = aws_instance.mysql.private_ip
+  }
+
+  provisioner "file" {
+  source      = "bootstrap.sh"           #Local file path
+  destination = "/tmp/bootstrap.sh"      #Destination path on remote machine
+}
+
+  provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh mysql"
+    ]
+  }
+}
+
+#rabbitmq
+resource "aws_instance" "rabbitmq" {
+    ami           = local.ami_id
+    instance_type = "t3.micro"
+    subnet_id = local.database_subnet_id
+    vpc_security_group_ids = [local.rabbitmq_sg_id]
+    tags = merge(
+        {
+            Name = "${var.project_name}-${var.environment}-rabbitmq"
+        },
+        local.common_tags,
+    )
+}
+
+
+resource "terraform_data" "bootstrap_rabbitmq" {
+  triggers_replace = [
+    aws_instance.rabbitmq.id
+  ]
+
+ connection {
+    type = "ssh"
+    user = "ec2-user"
+    password = "DevOps321"
+    host = aws_instance.rabbitmq.private_ip
+  }
+
+  provisioner "file" {
+  source      = "bootstrap.sh"           #Local file path
+  destination = "/tmp/bootstrap.sh"      #Destination path on remote machine
+}
+
+  provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh rabbitmq"
+    ]
+  }
+}
+
