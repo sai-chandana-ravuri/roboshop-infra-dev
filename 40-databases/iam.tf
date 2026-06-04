649d4d26b0a93cd17ba01f0d39d9f35b72f41589
sai-chandana-ravuri <saichandanaravuri@gmail.com>
2026-06-04 17:53:20 +0530

    roboshop-infra-dev

--- 40-databases/iam.tf
+++ 40-databases/iam.tf
@@ -24,3 +24,22 @@ resource "aws_iam_role" "mysql" {
         local.common_tags,
     )
 }
+
+
+resource "aws_iam_policy" "mysql" {
+  name        = local.mysql_policy_name
+  description = "A policy for MySQL Ec2 instance"
+  policy      = templatefile("mysql-iam-policy.json", {
+                environment = var.environment
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "mysql" {
+  role       = aws_iam_role.mysql.name
+  policy_arn = aws_iam_policy.mysql.arn
+}
+
+resource "aws_iam_instance_profile" "mysql" {
+  name = "${var.project_name}-${var.environment}-mysql"
+  role = aws_iam_role.mysql.name
+}
