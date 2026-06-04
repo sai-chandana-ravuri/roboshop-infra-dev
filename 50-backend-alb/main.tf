f6fd96c5839b4c710142cfefba6a0e6df6a47165
sai-chandana-ravuri <saichandanaravuri@gmail.com>
2026-06-05 00:39:14 +0530

    Created backend-alb

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -18,7 +18,7 @@ resource "aws_lb" "backend_alb" {
 
 #listener
 resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.front_end.arn
+  load_balancer_arn = aws_lb.backend_alb.arn
   port              = "80"
   protocol          = "HTTP"
 
