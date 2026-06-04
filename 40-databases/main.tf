7da446de12107fe8df781d497ab10275cf0913a6
sai-chandana-ravuri <saichandanaravuri@gmail.com>
2026-06-04 14:32:16 +0530

    bastion-server-provisoner-creation

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -21,7 +21,7 @@ resource "terraform_data" "bootstrap" {
     type = "ssh"
     user = "ec2-user"
     password = "DevOps321"
-    host = self.public_ip
+    host = aws_instance.mongodb.private_ip
   }
 
   provisioner "file" {
