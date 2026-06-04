676e692d71b9daa9b640804224a640a8412718a6
sai-chandana-ravuri <saichandanaravuri@gmail.com>
2026-06-04 15:37:57 +0530

    Addition of bootstrap.sh

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,1 +1,13 @@
-# We will insatll ansible here and configure mongodb. Copy this script to mongodb
+# We will insatll ansible here and configure mongodb. Copy this script to mongodb, execute trhough remote-exec
+
+#!/bin/bash
+
+component=$1
+dnf install ansible -y
+
+cd /home/ec2-user
+git clone https://github.com/sai-chandana-ravuri/ansible-roboshop-roles-tf.git
+
+cd ansible-roboshop-roles-tf
+
+ansible-playbook -e component=$component roboshop.yaml
