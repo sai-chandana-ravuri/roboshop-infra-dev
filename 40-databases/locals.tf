f24f72c18a7b4f79e661fc5769462bd8be500c53
sai-chandana-ravuri <saichandanaravuri@gmail.com>
2026-06-04 16:04:46 +0530

    Addition of redis

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -1,7 +1,8 @@
 locals {
     ami_id = data.aws_ami.joindevops.id
-    database_subnet_ids_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
+    database_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+    redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
     common_tags = {
         Project = var.project_name
         Environment = var.environment
--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z08145131ZQYG4A7JX2E8"
+}
+
+variable "domain_name" {
+    default = "daws88c.online"
+}
