637fc7ef7ec35b4ea1e471005df9d26bbe89a0ab
sai-chandana-ravuri <saichandanaravuri@gmail.com>
2026-06-04 14:30:47 +0530

    bastion-server-provisoner-creation

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -1,7 +1,7 @@
 locals {
     ami_id = data.aws_ami.joindevops.id
     database_subnet_ids_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
-    mongodb_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
+    mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
     common_tags = {
         Project = var.project_name
         Environment = var.environment
