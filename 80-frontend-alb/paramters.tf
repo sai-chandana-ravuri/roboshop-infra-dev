0bd95c58477c5d9c0e19137ebc21ddc2decd589a
sai-chandana-ravuri <saichandanaravuri@gmail.com>
2026-06-06 15:55:56 +0530

    robohop-infra

--- 80-frontend-alb/paramters.tf
+++ 80-frontend-alb/paramters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "frontend_alb_id" {
-    name  = "/${var.project_name}/${var.environment}/frontend_alb_id"
+    name  = "/${var.project_name}/${var.environment}/frontend_alb_listener_arn"
     type  = "String"
     value = aws_lb_listener.https.arn
 }
