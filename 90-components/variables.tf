9d2060bd95ff82406c9e59480363d9aa8d7129fd
sai-chandana-ravuri <saichandanaravuri@gmail.com>
2026-06-06 15:21:10 +0530

    robohop-infra

--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -3,19 +3,19 @@ variable "components" {
         #this is attaching to backend_alb, these are all backend components.
         catalogue = {
             rule_priority: 10 
-        }
+        },
         cart = {
             rule_priority: 20
-        }
+        },
         shipping = {
             rule_priority: 30
-        }
+        },
         user = {
             rule_priority: 40 
-        }
+        },
         payment = {
             rule_priority: 50
-        }
+        },
         #this is attaching to frontend_alb, there is only one component.
         frontend = {
             rule_priority: 10 
